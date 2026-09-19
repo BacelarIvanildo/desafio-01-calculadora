fe7be965f6479f8d0f0b1d5eac112eece18346df
Ivanildo Bacelar <117852620+BacelarIvanildo@users.noreply.github.com>
2026-09-19 12:56:20 -0300

    feat: calculadora

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Content, Row} from "./styles";
+import Input from "./components/Input";
+import Button from "./components/Button";
+import * as math from 'mathjs';
 
-function App() {
+import {useState} from 'react';
+
+const App = () => {
+  const [currentFormula, setCurrentFormula] = useState('');
+  const [currentNumber, setCurrentNumber] = useState('0');
+
+  const handleAddNumber = (number) => {
+    setCurrentNumber(prev =>`${prev === '0' ? '' : prev}${number}`);
+  }
+  
+  const handleClear = () => {
+    setCurrentNumber('0');
+  }
+
+  const handleAddOperation = (operation) => {
+    setCurrentFormula(prev => {
+      if(prev === '' || prev.endsWith('=')) {
+        return `${currentNumber}${operation}`;
+      }
+      return `${prev}${currentNumber}${operation}`;
+    });
+    setCurrentNumber('0');
+  }
+  const handleCalculate = () => {
+    if (currentFormula === '' || currentFormula.endsWith('=')) {
+      return;
+    }
+    setCurrentFormula(prev => `${prev}${currentNumber}`);
+    const result = math.evaluate(`${currentFormula}${currentNumber}`);
+    setCurrentFormula(prev => `${prev}=`);
+    setCurrentNumber(result.toString());
+  } 
+
+  const handleClearAll = () => {
+    setCurrentNumber('0');
+    setCurrentFormula('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Content>
+        <Input value={currentFormula} />
+        <Input value={currentNumber} />
+        <Row>
+          <Button label="*" onClick={() => handleAddOperation('*')} />
+          <Button label="/" onClick={() => handleAddOperation('/')} />
+          <Button label="C" onClick={handleClear} />
+          <Button label="AC" onClick={handleClearAll} />
+        </Row>
+        <Row>
+          <Button label="7" onClick={() => handleAddNumber('7')} />
+          <Button label="8" onClick={() => handleAddNumber('8')} />
+          <Button label="9" onClick={() => handleAddNumber('9')} />
+          <Button label="-" onClick={() => handleAddOperation('-')} />
+        </Row>
+        <Row>
+          <Button label="4" onClick={() => handleAddNumber('4')} />
+          <Button label="5" onClick={() => handleAddNumber('5')} />
+          <Button label="6" onClick={() => handleAddNumber('6')} />
+          <Button label="+" onClick={() => handleAddOperation('+')} />
+        </Row>
+        <Row>
+          <Button label="1" onClick={() => handleAddNumber('1')} />
+          <Button label="2" onClick={() => handleAddNumber('2')} />
+          <Button label="3" onClick={() => handleAddNumber('3')} />
+          <Button label="=" onClick={() => handleCalculate()} />
+        </Row>
+      </Content>
+    </Container>
   );
-}
+};
 
 export default App;
---
+++ src/components/Button/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #2b007c;
+    margin: 1px;
+    min-width: 60px;
+    font-size: 24px;
+    font-weight: 700;
+    font-family: "Roboto", sans-serif;
+    background-color: #2b007c;
+    color: #FFFFFF;
+    flex: 1;
+
+    &:hover {
+        opacity: 0.8;
+    }
+`
+
